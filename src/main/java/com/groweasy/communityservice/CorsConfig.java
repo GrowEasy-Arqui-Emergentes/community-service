03cc30865d16cecedea38c1148348a3639620ede
MartinCifuentes <103695203+MartinCifuentes@users.noreply.github.com>
2024-11-18 23:58:37 -0500

    Update CorsConfig.java

--- src/main/java/com/groweasy/communityservice/CorsConfig.java
+++ src/main/java/com/groweasy/communityservice/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
